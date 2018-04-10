f7bf7064cfa0ba6f2330538bfb30730781ff6920
dsharp <dsshar@gmail.com>
2018-04-10 18:07:52 -0400

    started work on simon says project, general shape of board is done

---
+++ SimonSays/SimonSays.js
@@ -0,0 +1,21 @@
+var sound1 = "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3";
+var sound2 = "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3";
+var sound3 = "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3";
+var sound4 = "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3";
+
+//document.getElementById('buttonOne').play();
+/*
+var timeout = null;
+timeout = setTimeout(() =>{
+    alert("hi");
+  }, 4000);
+$('button').click(() => {
+  var that = this;
+  console.log("before if, timeout is " + timeout);
+  if(timeout !== null) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(() =>{
+    alert("hi");
+  }, 4000);
+});*/
